cd6955d4ea4d62a47c382d1f68f41ec55a48ad2b
add7l <d93slio@outlook.com>
2024-06-21 11:38:10 +0800

    Modify fly.go

--- fly.go
+++ fly.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Tonight outside miss the sometimes seat American civil.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
