15e3f9b5385ef84d1f7074ea7a1d03ab61f97961
add7l <d93slio@outlook.com>
2024-07-16 10:18:45 +0800

    Modify affect.cpp

--- affect.cpp
+++ affect.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Result student economic cause usually less thus morning.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
