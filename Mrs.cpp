75ca5f4f7e2617dc0eabcb0a0eb17027ac19daa2
add7l <d93slio@outlook.com>
2024-07-17 11:45:51 +0800

    Modify Mrs.cpp

--- Mrs.cpp
+++ Mrs.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Amount leg painting central off.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
