229d16c42bf4c18bacb3d9f835a284edd15560b9
add7l <d93slio@outlook.com>
2024-07-20 13:54:35 +0800

    Modify affect.cpp

--- affect.cpp
+++ affect.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Civil rock collection exactly news front market.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
