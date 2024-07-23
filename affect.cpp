708c82a04acfbbe5a257b54b146349e55cb45c44
add7l <d93slio@outlook.com>
2024-07-23 13:31:28 +0800

    Modify affect.cpp

--- affect.cpp
+++ affect.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Interesting someone century answer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
