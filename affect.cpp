6111313883cd336a58ff247e5437ed7993751068
add7l <d93slio@outlook.com>
2024-08-04 12:41:40 +0800

    Modify affect.cpp

--- affect.cpp
+++ affect.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Financial partner technology let quality.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
