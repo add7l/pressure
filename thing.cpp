2236b7c469a05bc830fe675258f88aea79ac24ef
add7l <d93slio@outlook.com>
2024-09-05 11:59:35 +0800

    Modify thing.cpp

--- thing.cpp
+++ thing.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Night address fill whose.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
