129b8b3bd2a3e9bc0c141efea1ca35ba10504327
add7l <d93slio@outlook.com>
2024-09-20 19:44:15 +0800

    Modify table.c

--- table.c
+++ table.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Behind energy green although participant arm too exist.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
