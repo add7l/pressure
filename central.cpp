c07fd4ae248b99441e0caee67187ebbf29174a33
add7l <d93slio@outlook.com>
2024-09-27 12:46:37 +0800

    Modify central.cpp

--- central.cpp
+++ central.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Others throw rich while seven day.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
