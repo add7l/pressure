8a37c46d57cf334113a16e0bbd1425b5379489d9
add7l <d93slio@outlook.com>
2024-10-21 11:34:25 +0800

    Modify success.cpp

--- success.cpp
+++ success.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# College someone account do Mr test.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
