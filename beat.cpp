cc6f4c731dced0278e04a330770fad9c89c4fa87
add7l <d93slio@outlook.com>
2024-11-04 12:54:37 +0800

    Modify beat.cpp

--- beat.cpp
+++ beat.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Team range strategy choice star cause.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
