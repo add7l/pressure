a6038746f72fb44b6b562dfbd7b97c54f2caeba0
add7l <d93slio@outlook.com>
2025-02-14 12:48:57 +0800

    Modify mission.c

--- mission.c
+++ mission.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Outside difficult tonight participant raise national.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
