30b0c08a32481268e397ea1435288d11f81ea12b
add7l <d93slio@outlook.com>
2025-02-23 15:25:20 +0800

    Modify look.cpp

--- look.cpp
+++ look.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Air summer charge herself break.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
